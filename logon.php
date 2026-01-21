bcfdb6e01e31943182ea175c2d01d9b4e29baaae
Eric Ananga <ericsurvival363@gmail.com>
2026-01-21 23:39:08 +0000

    A simple login form

---
+++ logon.php
@@ -0,0 +1,26 @@
+<html>
+    <body>
+        <h3>Member Login </h3>
+        <pre>
+<form name ="Logon" action = "welcome.php" method = "post">
+<table border="0">
+    <tr>
+        <td>UserName:</td>
+        <td><input type = "text" name = "username" size ="20" required ></td>
+    </tr>
+    <tr>
+        <td>Password:</td>
+        <td><input type ="password" name = "submit" value = "submit" size = "10"></td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td><input type = "submit" name = "submit" value = "submit"></td>
+    </tr>
+</table>
+</form>
+
+
+
+        </pre>
+    </body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+
+    <?php
+    //display the username entered on the form
+    $Username = $_POST["username"];
+    echo "Welcome, $Username!";
+    
+    ?>
+    </body>
+</html>
